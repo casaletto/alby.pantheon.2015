07232dbab870e4086c938c0912d7209982fa2cd1
casaletto <casaletto@users.noreply.github.com>
2015-06-05 14:50:08 +1000

    pantheon code gen unit test: works !

---
+++ alby.pantheon.codegen/codegen.cmd
@@ -0,0 +1,9 @@
+c:
+
+cd c:\albyStuff\development\alby.pantheon.2015\alby.pantheon.codegen
+
+c:\albyStuff\development\alby.codegen.2015\bin\alby.codegen.generator.2015.exe codegen.xml --ri --unit-tests --queries --tables --views --storedProcs --verbose
+
+pause
+
+
